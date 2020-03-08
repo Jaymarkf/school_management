415f5d5c53de785c23bd08d37037169baf44c617
maxx <fernandesjaymark@yahoo.com>
2020-03-08 11:56:36 +0800

    database new

--- database_2.sql
+++ database_2.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `academic_settings`
 --
-
+create database pro_sms;
+use pro_sms;
 DROP TABLE IF EXISTS `academic_settings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
